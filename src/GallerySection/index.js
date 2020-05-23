f559140f6da9ec9feb40d830cf39cacfa9f219fc
NSkye <nikolai.laevskii@yandex.ru>
2020-05-23 09:44:21 +0300

    Add comments for GallerySection

--- src/GallerySection/index.js
+++ src/GallerySection/index.js
@@ -3,6 +3,7 @@ import calculateContainerSize from './calculateContainerSize'
 
 class GallerySection {
   constructor({
+    // Text for section's header
     headerText,
     // Array of image links
     imageLinks,
@@ -17,30 +18,45 @@ class GallerySection {
     // Container's offset from stage top
     globalOffsetY
   }) {
+    // Indicator that some images are currently loading
     this.imagesAreLoading = false
+
+    // Pool of images that are waiting to be loaded
     this.loadingQueue = []
+
+    // Images by id
     this.images = {}
+
+    // Links for each image
+    this.links = imageLinks
 
     this.imageHeight = imageHeight
     this.imageWidth = imageWidth
     this.globalOffsetY = globalOffsetY
+    this.offsetToLoad = offsetToLoad
 
+    // Assets loader for image loading
     this.loader = new PIXI.Loader()
+
+    // Container instance that will represent section
     this.container = new PIXI.Container()
     this.container.interactive = true
     this.container.buttonMode = true
     this.container.defaultCursor = 'pointer'
 
+    // Section's header with text
     this.header = new PIXI.Text(headerText, {
       fontFamily: 'Arial',
       fontWeight: '600'
     })
-    this.localOffset = this.header.height + 10
-    this.container.addChild(this.header)
     this.header.y = 10
     this.header.x = 45
-    this.links = imageLinks
+
+    // Space occupied by header
+    this.localOffset = this.header.height + 10
+    this.container.addChild(this.header)
   
+    // Container's virtual size allows to place images correctly
     const { imagesPerLine, linesPerStage, width, height } = calculateContainerSize({
       imageCount: this.links.length,
       imageHeight: this.imageHeight,
@@ -52,11 +68,16 @@ class GallerySection {
     this.width = width
     this.height = height + this.localOffset
 
+    // Create image instances and push then to this.images
     this.initializeImages()
+
     this.setMaxWidth(maxWidth)
-    this.offsetToLoad = offsetToLoad
   }
 
+  /**
+   * Creates wrapper for each image
+   * and pushes them to this.images
+   */
   initializeImages() {
     this.images = this.links.reduce((images, link, index) => {
       const image = this.calculateImagePosition(index)
@@ -70,6 +91,11 @@ class GallerySection {
     }, {})
   }
 
+  /**
+   * Determine image position in gallery
+   * @param {number} index image's index in this.links or it's id
+   * @returns {{ x: number, y: number, line: number, column: number }}
+   */
   calculateImagePosition(index) {
     const line = Math.floor(index / this.imagesPerLine)
     const column = index % this.imagesPerLine
@@ -80,12 +106,23 @@ class GallerySection {
     return { line, column, x, y }
   }
 
+  /**
+   * Detects if image should be visible to user
+   * (or at least will sooon be visible) based on his
+   * vertical scroll position
+   * @param {number} yCoordinate images global Y coordinate
+   */
   calculateImageVisibility(yCoordinate) {
     const windowBottom = window.scrollY + window.innerHeight
     return (yCoordinate - windowBottom) < this.offsetToLoad &&
       (window.scrollY - yCoordinate) < this.offsetToLoad
   }
 
+  /**
+   * Update .visibility property of all
+   * images in container based on user's
+   * scroll position
+   */
   updateImagesVisibility() {
     this.links.map((_, index) => {
       const image = this.images[index]
@@ -106,6 +143,12 @@ class GallerySection {
     })
   }
 
+  /**
+   * Update all images { x, y } positions
+   * according to width of container
+   * Will be called on initialization
+   * and on window resize
+   */
   updateImagesPositioning() {
     this.links.map((_, index) => {
       const image = this.images[index]
@@ -119,6 +162,12 @@ class GallerySection {
     })
   }
 
+  /**
+   * Start loading all images that are currently
+   * visible to user, images are taken from loadingQueue
+   * by small chunks, function will be rerunning until
+   * images are loaded
+   */
   loadVisibleImages() {
     if (!this.loadingQueue.length) {
       this.imagesAreLoading = false
@@ -142,12 +191,23 @@ class GallerySection {
       .then(() => this.loadVisibleImages())
   }
 
+  /**
+   * Load images from provided urls, returns
+   * promis that will be resolved when all
+   * images are loaded
+   * @param {string[]} links image urls
+   * @returns {Promise}
+   */
   loadImages(links) {
     return new Promise(resolve => this.loader
       .add(links)
       .load(() => resolve()))
   }
 
+  /**
+   * Create sprite from already loaded image
+   * @param {object} image wrapped image from .images
+   */
   createSprite(image) {
     const rectangle = new PIXI.Rectangle(0, 0, 45, 45)
     const texture = this.loader.resources[image.link].texture
@@ -173,6 +233,11 @@ class GallerySection {
     return sprite
   }
 
+  /**
+   * Change max width of container with
+   * recalculation of images positions
+   * @param {number} maxWidth
+   */
   setMaxWidth(maxWidth) {
     const { imagesPerLine, linesPerStage, width, height } = calculateContainerSize({
       imageCount: this.links.length,
@@ -189,12 +254,21 @@ class GallerySection {
     this.updateImagesPositioning()
   }
 
+  /**
+   * Change container's vertical position on stage
+   * @param {number} y Y-coordinate
+   */
   setGlobalOffsetY(y) {
     this.globalOffsetY = y
     this.container.position.y = y
     this.recalculateVisibleImages()
   }
 
+  /**
+   * Recalculate which images should be visible
+   * and if image loading is not in progress
+   * start it
+   */
   recalculateVisibleImages() {
     this.updateImagesVisibility()
     if (!this.imagesAreLoading) {
