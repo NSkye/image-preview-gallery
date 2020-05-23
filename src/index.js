dff737ee663da29f324b5e500972d0ed00664d64
NSkye <nikolai.laevskii@yandex.ru>
2020-05-23 12:11:35 +0300

    Add more diverse sections

--- src/index.js
+++ src/index.js
@@ -7,9 +7,12 @@ import Gallery from './Gallery'
 const IMAGE_HOST = 'http://127.0.0.1:3999'
 
 const imagesByYear = [
-  ['2018', Array.from(generateImageLinks(IMAGE_HOST, 'jpg', 300, 0))],
+  ['2015', Array.from(generateImageLinks(IMAGE_HOST, 'jpg', 25, 0))],
+  ['2016', Array.from(generateImageLinks(IMAGE_HOST, 'jpg', 50, 300))],
+  ['2017', Array.from(generateImageLinks(IMAGE_HOST, 'jpg', 100, 600))],
+  ['2018', Array.from(generateImageLinks(IMAGE_HOST, 'jpg', 200, 0))],
   ['2019', Array.from(generateImageLinks(IMAGE_HOST, 'jpg', 300, 300))],
-  ['2020', Array.from(generateImageLinks(IMAGE_HOST, 'jpg', 400, 600))],
+  ['2020', Array.from(generateImageLinks(IMAGE_HOST, 'jpg', 325, 600))]
 ]
 
 new Gallery({ imagesByYear })
